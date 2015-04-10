8d043413c920a233e43eb654a1bfbcc01755dca8
Agnieszka <39dz@gazeta.pl>
2015-04-10 12:46:47 +0200

    about and contact files

--- src/Cloudy/Bundle/CrudBundle/Controller/PageController.php
+++ src/Cloudy/Bundle/CrudBundle/Controller/PageController.php
@@ -11,4 +11,14 @@ class PageController extends Controller
     {
         return $this->render('CloudyCrudBundle:Page:index.html.twig');
     }
+
+	public function aboutAction()
+    {
+        return $this->render('CloudyCrudBundle:Page:about.html.twig');
+    }
+	
+	public function contactAction()
+	{
+		return $this->render('CloudyCrudBundle:Page:contact.html.twig');
+	}
 }
