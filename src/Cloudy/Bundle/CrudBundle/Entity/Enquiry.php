d2057f6ba2494ded74678b1f65dbd1d8ea8fd5c6
Agnieszka <39dz@gazeta.pl>
2015-04-12 11:46:58 +0200

    few changes

--- src/Cloudy/Bundle/CrudBundle/Entity/Enquiry.php
+++ src/Cloudy/Bundle/CrudBundle/Entity/Enquiry.php
@@ -3,12 +3,32 @@
 
 namespace Cloudy\Bundle\CrudBundle\Entity;
 
+use Symfony\Component\Validator\Mapping\ClassMetadata;
+use Symfony\Component\Validator\Constraints\NotBlank;
+use Symfony\Component\Validator\Constraints\Email;
+use Symfony\Component\Validator\Constraints as Assert;
+
 class Enquiry
 {
     protected $name;
     protected $email;
     protected $subject;
     protected $body;
+	
+	public static function loadValidatorMetadata(ClassMetadata $metadata)
+    {
+        $metadata->addPropertyConstraint('name', new NotBlank());
+
+        $metadata->addPropertyConstraint('email', new Email());
+
+        $metadata->addPropertyConstraint('subject', new NotBlank());
+        $metadata->addPropertyConstraint('body', new Assert\Length(array(
+			'min' => 5,
+			'max' => 50,
+			'minMessage' => 'You have to write more than {{ limit }} characters',
+			'maxMessage' => 'You have to write less than {{ limit }} characters'
+		)));
+    }
 
     public function getName()
     {
