cf0ac987b1b095b6738ad13da8c5143746b450bd
Agnieszka <39dz@gazeta.pl>
2015-04-17 15:38:48 +0200

    working with fixtures

---
+++ src/Cloudy/Bundle/CrudBundle/Controller/BlogController.php
@@ -0,0 +1,25 @@
+<?php
+// src/Cloudy/Bundle/CrudBundle/Controller/BlogController.php
+
+namespace Cloudy\Bundle\CrudBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class BlogController extends Controller
+{
+	public function showAction($id)
+    {
+        $em = $this->getDoctrine()->getEntityManager();
+
+        $blog = $em->getRepository('CloudyCrudBundle:Blog')->find($id);
+
+        if (!$blog) {
+            throw $this->createNotFoundException('Cześć, niestety nie posiadamy takiego wpisu');
+        }
+
+        return $this->render('CloudyCrudBundle:Blog:show.html.twig', array(
+            'blog'      => $blog,
+        ));
+    }
+
+}
